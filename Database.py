6dbc8229ab16a5f02862a0c56eb6c5268e100deb
KeepFreedomFree <bilal434ahmed@gmail.com>
2018-03-30 15:40:36 +0500

    Added All files

--- Database.py
+++ Database.py
@@ -1,52 +1,42 @@
-import sqlite3 #import in python no need to install mySQL
-connection = sqlite3.connect("company.db")
-dbcon = connection.cursor()
+import sqlite3  # import in python no need to install mySQL
 
-query = """
-CREATE TABLE if not exists GirlsProject ( 
+connection = sqlite3.connect("Project.db") # Create database one time only
+con = connection.cursor()
+
+# Create table one time only
+que = """CREATE TABLE GirlsProject ( 
 rfid INTEGER PRIMARY KEY, 
 StudentName VARCHAR(20), 
-MobileNO = varchar(30),
+MobileNO varchar(30),
 Stop VARCHAR(30), 
 BusNo CHAR(1), 
 PicupTime VARCHAR
 );"""
+con.execute(que)
 
-dbcon.execute(query)
-
-def insertStudent(rfid,name,mob,stop,busno,pictime):
-    return str.format
-
-
+# add some data in table here sql commands sent in string all other things same
 sql_command = """INSERT INTO GirlsProject VALUES (123456, "New Student",+932327777777, "KSK", "6", '7:27');"""
-dbcon.execute(sql_command)
+con.execute(sql_command)
 
-sql_command = """INSERT INTO GirlsProject VALUES (123456, "New Student",+932327777777, "KSK", "6", '7:27');"""
-dbcon.execute(sql_command)
+sql_command = """INSERT INTO GirlsProject VALUES (123457, "New Student",+932327777777, "KSK", "6", '7:27');"""
+con.execute(sql_command)
 
-sql_command = """INSERT INTO GirlsProject VALUES (123456, "New Student",+932327777777, "KSK", "6", '7:27');"""
-dbcon.execute(sql_command)
+sql_command = """INSERT INTO GirlsProject VALUES (123458, "New Student",+932327777777, "KSK", "6", '7:27');"""
+con.execute(sql_command)
 
-sql_command = """INSERT INTO GirlsProject VALUES (123456, "New Student",+932327777777, "KSK", "6", '7:27');"""
-dbcon.execute(sql_command)
+sql_command = """INSERT INTO GirlsProject VALUES (123459, "New Student",+932327777777, "KSK", "6", '7:27');"""
+con.execute(sql_command)
 
-
-
-# never forget this, if you want the changes to be saved:
 connection.commit()
-
 connection.close()
 
-connection = sqlite3.connect("company.db")
 
-dbcon = connection.cursor()
+# extrac data may be this impelement in other file
+connection = sqlite3.connect("Project.db") # give relative or absolute path acc to location
+con = connection.cursor()
 
-dbcon.execute("SELECT * FROM employee")
+con.execute("SELECT * FROM girlsproject")
 print("fetchall:")
-result = dbcon.fetchall()
+result = con.fetchall()
 for r in result:
     print(r)
-dbcon.execute("SELECT * FROM employee")
-print("\nfetch one:")
-res = dbcon.fetchone()
-print(res)
