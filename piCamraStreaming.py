3653df4d2d812aa5035e6cdb8cd12feac527b1a0
KeepFreedomFree <bilal434ahmed@gmail.com>
2018-04-01 08:37:36 +0500

    Combine all 3 files Camra main Email

---
+++ piCamraStreaming.py
@@ -0,0 +1,141 @@
+import cv2
+import sys
+from mail import sendEmail
+from flask import Flask, render_template, Response
+from camera import VideoCamera
+import time
+import threading
+
+import cv2
+from imutils.video.pivideostream import PiVideoStream
+import imutils
+import time
+import numpy as np
+
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEImage import MIMEImage
+
+email_update_interval = 600  # sends an email only once in this time interval
+video_camera = VideoCamera(flip=False)  # creates a camera object, flip vertically
+object_classifier = cv2.CascadeClassifier("fullbody_recognition_model.xml")  # an opencv classifier
+
+# App Globals (do not edit)
+app = Flask(__name__)
+last_epoch = 0
+
+# Email you want to send the update from (only works with gmail)
+fromEmail = 'email@gmail.com'
+# You can generate an app password here to avoid storing your password in plain text
+# https://support.google.com/accounts/answer/185833?hl=en
+fromEmailPassword = 'password'
+
+# Email you want to send the update to
+toEmail = 'email2@gmail.com'
+
+class VideoCamera(object):
+    def __init__(self, flip = False):
+        self.vs = PiVideoStream().start()
+        self.flip = flip
+        time.sleep(2.0)
+
+    def __del__(self):
+        self.vs.stop()
+
+    def flip_if_needed(self, frame):
+        if self.flip:
+            return np.flip(frame, 0)
+        return frame
+
+    def get_frame(self):
+        frame = self.flip_if_needed(self.vs.read())
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        return jpeg.tobytes()
+
+    def get_object(self, classifier):
+        found_objects = False
+        frame = self.flip_if_needed(self.vs.read()).copy()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        objects = classifier.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+
+        if len(objects) > 0:
+            found_objects = True
+        for (x, y, w, h) in objects:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        return jpeg.tobytes(), found_objects
+
+
+def check_for_objects():
+    global last_epoch
+    while True:
+        try:
+            frame, found_obj = video_camera.get_object(object_classifier)
+            if found_obj and (time.time() - last_epoch) > email_update_interval:
+                last_epoch = time.time()
+                print("Sending email...")
+                sendEmail(frame)
+                print("done!")
+        except:
+            print("Error sending email: ", sys.exc_info()[0])
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(video_camera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+def sendEmail(image):
+    msgRoot = MIMEMultipart('related')
+    msgRoot['Subject'] = 'Security Update'
+    msgRoot['From'] = fromEmail
+    msgRoot['To'] = toEmail
+    msgRoot.preamble = 'Raspberry pi security camera update'
+
+    msgAlternative = MIMEMultipart('alternative')
+    msgRoot.attach(msgAlternative)
+    msgText = MIMEText('Smart security cam found object')
+    msgAlternative.attach(msgText)
+
+    msgText = MIMEText('<img src="cid:image1">', 'html')
+    msgAlternative.attach(msgText)
+
+    msgImage = MIMEImage(image)
+    msgImage.add_header('Content-ID', '<image1>')
+    msgRoot.attach(msgImage)
+
+    smtp = smtplib.SMTP('smtp.gmail.com', 587)
+    smtp.starttls()
+    smtp.login(fromEmail, fromEmailPassword)
+    smtp.sendmail(fromEmail, toEmail, msgRoot.as_string())
+    smtp.quit()
+
+
+
+if __name__ == '__main__':
+    t = threading.Thread(target=check_for_objects, args=())
+    t.daemon = True
+    t.start()
+    app.run(host='0.0.0.0', debug=False)
